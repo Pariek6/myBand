b303ac295d50ebdb2977ea10e755f22e546c297c
Pariek6 <patrickfrumau@hotmail.com>
2017-06-23 13:43:21 +0200

    Add files via upload

---
+++ code/index.php
@@ -0,0 +1,52 @@
+<?php
+
+//configuration settings
+require 'includes/config.php';
+
+//initialisation
+require 'includes/bootstrap.php';
+
+// head
+
+$templateParser->display('head.tpl');
+
+// header
+$templateParser->display('header.tpl');
+
+// menu
+include 'views/menu.tpl';
+
+
+
+$action = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+switch ($action) {
+    case 'home':
+        include 'views/home.tpl';
+
+        break;
+    case 'articles':
+        //get articles from the model
+        require_once 'model/getarticles.php';
+
+        $templateParser->assign('article_list', $result_list);
+        $templateParser->display('articles.tpl');
+        break;
+    case 'about':
+        require_once('model/getabout.php');
+        $templateParser->assign('about_list', $result_list);
+        //use 'display' to show a template
+        $templateParser->display('about.tpl');
+        break;
+    case 'contact':
+        //contact
+        $templateParser->display('contact.tpl');
+        break;
+}
+
+
+// footer
+$templateParser->assign('footer', $footer);
+$templateParser->display('footer.tpl');
+
+?>

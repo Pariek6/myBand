3d3bf44ec42e1ff5a307eb9223124b14265fc3ff
Pariek6 <patrickfrumau@hotmail.com>
2017-06-23 13:47:23 +0200

    Add files via upload

---
+++ code/includes/config.php
@@ -0,0 +1,15 @@
+<?php
+$titlearticles = 'Articles';
+$about = 'This is about my page';
+$articles = 'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean
+    collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.';
+$menu = '';
+$contact = 'Create your contact page here';
+$footer = 'This is a project from school called: MyBand';
+
+define('DB_HOST','localhost');
+define('DB_NAME','24011_aliendatabase');
+define('DB_USERNAME','24011_alien');
+define('DB_PASSWORD','24011_alien');
+
+?>
